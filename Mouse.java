e40c5507d335bbb9a366e387f02da16fa40cb861
yokeshTC <112368654+yokeshTC@users.noreply.github.com>
2024-03-21 08:33:46 +0530

    Mouse.java

--- Mouse.java
+++ Mouse.java
@@ -2,39 +2,48 @@ import java.awt.Frame;
 import java.awt.TextArea;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-public class Mouse{
-    public static void main(String arg[]) {
-        Mouse a= new Mouse();
+
+public class Mouse {
+    public static void main(String[] args) {
+        Mouse a = new Mouse();
     }
-    public class U515Mouse implements MouseListener {
-        TextArea s;
-        public void Mouse()
-        {
-            Frame d=new Frame("kkkk");
-            s=new TextArea("");
-            d.add(s);
-            s.addMouseListener(this);
-            d.setSize(200, 200);
-            d.show();
-        }
+
+    public Mouse() {
+        Frame d = new Frame("Mouse Event Listener");
+        TextArea s = new TextArea("");
+        d.add(s);
+        s.addMouseListener(new U515Mouse());
+        d.setSize(200, 200);
+        d.setVisible(true);
+    }
+
+    public static class U515Mouse implements MouseListener {
+        @Override
         public void mousePressed(MouseEvent e) {
             System.out.println("MousePressed");
-            int a=e.getX();
-            int b=e.getY();
-            System.out.println("X="+a+"Y="+b);
+            int x = e.getX();
+            int y = e.getY();
+            System.out.println("X=" + x + " Y=" + y);
         }
+
+        @Override
         public void mouseReleased(MouseEvent e) {
             System.out.println("MouseReleased");
         }
+
+        @Override
         public void mouseEntered(MouseEvent e) {
             System.out.println("MouseEntered");
         }
+
+        @Override
         public void mouseExited(MouseEvent e) {
             System.out.println("MouseExited");
         }
+
+        @Override
         public void mouseClicked(MouseEvent e) {
             System.out.println("MouseClicked");
         }
-
     }
 }
